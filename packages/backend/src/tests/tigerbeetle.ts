094906cbb51c5e860976953669a084a6c70796f6
Brandon Wilson <brandon@coil.com>
2022-03-11 17:42:25 -0600

    chore(backend): stream tigerbeetle container logs

--- packages/backend/src/tests/tigerbeetle.ts
+++ packages/backend/src/tests/tigerbeetle.ts
@@ -25,7 +25,7 @@ export async function startTigerbeetleContainer(
     .withWaitStrategy(Wait.forLogMessage(/initialized data file/))
     .start()
 
-  return await new GenericContainer(
+  const tigerbeetleContainer = await new GenericContainer(
     'ghcr.io/coilhq/tigerbeetle@sha256:e2e9717c7f9bb916c9a1b7904fc5c84a522d6346efeacc65c2b19c204e50e4a3'
   )
     .withExposedPorts(TIGERBEETLE_PORT)
@@ -39,4 +39,12 @@ export async function startTigerbeetleContainer(
     ])
     .withWaitStrategy(Wait.forLogMessage(/listening on/))
     .start()
+
+  const stream = await tigerbeetleContainer.logs()
+  stream
+    .on('data', (line) => console.log(line))
+    .on('err', (line) => console.error(line))
+    .on('end', () => console.log('Stream closed'))
+
+  return tigerbeetleContainer
 }
