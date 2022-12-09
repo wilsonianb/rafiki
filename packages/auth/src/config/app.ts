645ee21ce301737ae9a70f25a5363d2d9e95b753
Brandon Wilson <brandon@coil.com>
2022-12-09 16:23:10 -0600

    chore(auth): remove unused INTROSPECTION_HTTPSIG

--- packages/auth/src/config/app.ts
+++ packages/auth/src/config/app.ts
@@ -41,7 +41,6 @@ export const Config = {
   accessTokenExpirySeconds: envInt('ACCESS_TOKEN_EXPIRY_SECONDS', 10 * 60), // Default 10 minutes
   databaseCleanupWorkers: envInt('DATABASE_CLEANUP_WORKERS', 1),
   accessTokenDeletionDays: envInt('ACCESS_TOKEN_DELETION_DAYS', 30),
-  introspectionHttpsig: envBool('INTROSPECTION_HTTPSIG', false),
   incomingPaymentInteraction: envBool('INCOMING_PAYMENT_INTERACTION', false),
   bypassSignatureValidation: envBool('BYPASS_SIGNATURE_VALIDATION', false)
 }
